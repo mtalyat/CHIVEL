3ac8e9c01537f98fe17ea9983a9ab2453f25e04a
Mitchell Talyat <mitchell.talyat@gmail.com>
2025-06-25 23:42:20 -0500

    Remove examples

--- Examples/test.py
+++
@@ -1,9 +0,0 @@
-import chivel
-
-# set cwd to the directory containing this file
-import os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-print("Recording started. Press F12 to stop.")
-chivel.record('recording.py')
-print("Recording stopped.")
