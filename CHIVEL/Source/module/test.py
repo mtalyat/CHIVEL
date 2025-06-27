fab90e9a7f3b125433e39bb54bb618233cceca8f
Mitchell Talyat <mitchell.talyat@gmail.com>
2025-06-27 05:54:15 -0500

    Update test.py

--- CHIVEL/Source/module/test.py
+++ CHIVEL/Source/module/test.py
@@ -2,10 +2,10 @@ import chivel
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-screen = chivel.capture(0)
+screen = chivel.capture()
 size = screen.get_size()
 print(f"Captured screen size: {size[0]}x{size[1]}")
-print(f"Display rect: {chivel.display_get_rect(0)}")
+print(f"Display rect: {chivel.display_get_rect()}")
 chivel.mouse_move(0, (10, 10))
 chivel.wait(1.0)
 print(f'Mouse moved to {chivel.mouse_get_location()}')
