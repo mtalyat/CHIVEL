c32cc22bf16562b8360bd9699333c286a8252a2f
Mitchell Talyat <mitchell.talyat@gmail.com>
2025-06-27 04:02:59 -0500

    Update test.py

--- CHIVEL/Source/module/test.py
+++ CHIVEL/Source/module/test.py
@@ -1,12 +1,14 @@
 import chivel
-
-# set cwd to the directory containing this file
 import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-print("Recording started. Press F12 to stop.")
-chivel.record('recording.py')
-print("Recording stopped.")
-print("Playing back the recording...")
-chivel.play('recording.py')
-print("Playback finished.")
+screen = chivel.capture(0)
+size = screen.get_size()
+print(f"Captured screen size: {size[0]}x{size[1]}")
+print(f"Display rect: {chivel.display_get_rect(0)}")
+chivel.mouse_move(0, (10, 10))
+chivel.wait(1.0)
+print(f'Mouse moved to {chivel.mouse_get_location()}')
+chivel.mouse_move(0, (size[0] - 10, size[1] - 10))
+chivel.wait(1.0)
+print(f'Mouse moved to {chivel.mouse_get_location()}')
